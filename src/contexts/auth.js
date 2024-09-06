79412c30f0639ee47b3c0601c316f425f93c396d
Rodrigo Lima <162887259+rodrigolima-dev@users.noreply.github.com>
2024-09-06 14:38:56 -0300

    contextos

--- src/contexts/auth.js
+++ src/contexts/auth.js
@@ -3,12 +3,11 @@ import React, { createContext, useState } from "react";
 export const AuthContext = createContext({});
 
 export default function AuthProvider ({ children }) {
-    const [ user, setUser ] = useState({
-        name: ' Matheus ',
-    });
+    const [ user, setUser ] = useState({});
 
     return (
-        <AuthContext.Provider value={{user}}>
+        //!! está convertendo user para booleano, se estiver null é false.
+        <AuthContext.Provider value={{signed: !!user, user}}>
             {children}
         </AuthContext.Provider>
     );
